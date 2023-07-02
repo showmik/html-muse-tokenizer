af56ff4d10a9a21ccdc255ef1ac7809c2530e508
Showmik Haque <showmik.haque@gmail.com>
2023-07-02 20:51:20 +0600

    Revert "Chor: Cleans up namespace"
    This reverts commit 9583897ab23e35f84dbaaec0e27bdb1d5249b31b.

--- HtmlMuse.Tokenizer/States/AfterAttributeValueQuotedState.cs
+++ HtmlMuse.Tokenizer/States/AfterAttributeValueQuotedState.cs
@@ -1,36 +1,37 @@
 ﻿using HtmlMuse.Tokenizer.Tokens;
 
-namespace HtmlMuse.Tokenizer.States;
-
-public class AfterAttributeValueQuotedState : State<HtmlTokenizer>
+namespace HtmlMuse.Tokenizer.States
 {
-    private static AfterAttributeValueQuotedState? _instance;
-    public static AfterAttributeValueQuotedState Instance => _instance ??= new AfterAttributeValueQuotedState();
-
-    public override void Execute(HtmlTokenizer owner)
+    public class AfterAttributeValueQuotedState : State<HtmlTokenizer>
     {
-        if (owner.CodePoint.IsWhiteSpace)
+        private static AfterAttributeValueQuotedState? _instance;
+        public static AfterAttributeValueQuotedState Instance => _instance ??= new AfterAttributeValueQuotedState();
+
+        public override void Execute(HtmlTokenizer owner)
         {
-            owner.FSM.SwitchState(BeforeAttributeNameState.Instance);
-        }
-        else if (owner.CodePoint.Value == '/')
-        {
-            owner.FSM.SwitchState(SelfClosingStartTagState.Instance);
-        }
-        else if (owner.CodePoint.Value == '>')
-        {
-            owner.EmitCurrentToken();
-            owner.FSM.SwitchState(DataState.Instance);
-        }
-        else if (owner.IsAtEndOfFile)
-        {
-            // Parse Error: eof-in-tag
-            owner.EmitToken(new EOFToken());
-        }
-        else
-        {
-            // Parse Error: missing-whitespace-between-attributes
-            owner.ReconsumeCharacter(BeforeAttributeNameState.Instance);
+            if (owner.CodePoint.IsWhiteSpace)
+            {
+                owner.FSM.SwitchState(BeforeAttributeNameState.Instance);
+            }
+            else if (owner.CodePoint.Value == '/')
+            {
+                owner.FSM.SwitchState(SelfClosingStartTagState.Instance);
+            }
+            else if (owner.CodePoint.Value == '>')
+            {
+                owner.EmitCurrentToken();
+                owner.FSM.SwitchState(DataState.Instance);
+            }
+            else if (owner.IsAtEndOfFile)
+            {
+                // Parse Error: eof-in-tag
+                owner.EmitToken(new EOFToken());
+            }
+            else
+            {
+                // Parse Error: missing-whitespace-between-attributes
+                owner.ReconsumeCharacter(BeforeAttributeNameState.Instance);
+            }
         }
     }
 }
