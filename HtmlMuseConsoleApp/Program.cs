242c8f451edcdc8eca53dffc64a4bbb0529b9a60
Showmik Haque <showmik.haque@gmail.com>
2023-07-02 20:56:17 +0600

    Chor: Adds console app project

---
+++ HtmlMuseConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using HtmlMuse.Tokenizer;
+
+namespace HtmlMuseConsoleApp;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string htmlCode = "<!DOCTYPE html><html><body><!-- HTML MUSE --><h1>Happy, <br/>Tokenizing!</h1></body></html>";
+
+        // Create an HtmlTokenizer instance
+        HtmlTokenizer tokenizer = new HtmlTokenizer(htmlCode);
+
+        // Retrieve all tokens
+        List<Token> tokens = tokenizer.GetAllTokens();
+
+        // Iterate through the tokens
+        foreach (Token token in tokens)
+        {
+            Console.WriteLine($"{token}");
+        }
+    }
+}
